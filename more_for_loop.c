f4f3f0d1051de5262761a8a8bf9645b399ede6d6
ijazhossain <ijaz.hossain35@gmail.com>
2025-04-09 11:44:12 +0600

    more for loop added

--- more_for_loop.c
+++ more_for_loop.c
@@ -1,1 +1,23 @@
-ß
+// for loop for odd number
+// #include<stdio.h>
+// int main(){
+//     for(int i=1; i<100;i+=2){
+//         printf("%d\n",i);
+//     }
+// }
+
+// for loop for even number
+// #include<stdio.h>
+// int main(){
+//         for(int i=2; i<=10;i+=2){
+//           printf("%d\n",i);
+//        }
+//      }
+
+// for loop for gunitok of 4 
+#include<stdio.h>
+int main(){
+        for(int i=2; i<=100;i=i*2){
+          printf("%d\n",i);
+       }
+     }
